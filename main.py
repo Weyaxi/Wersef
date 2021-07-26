d3fbaa2c1895115afdbc1a4fb7c9d2eaf10c24b1
Weyaxi <81961593+Weyaxi@users.noreply.github.com>
2021-07-26 13:15:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -819,7 +819,7 @@ async def yalvar_error(ctx, error):
 
 
 @bot.command(aliases=['tahminet', 'tahmin-et'])
-@commands.cooldown(1, 8, commands.BucketType.user)
+@commands.cooldown(1, 15, commands.BucketType.user)
 async def guess(ctx):
     emoji = "Hata"
 
@@ -2166,7 +2166,7 @@ async def yetkilerim(ctx):
 
 
 @bot.command(aliases=['mail-gönder', 'mailgönder'])
-@commands.cooldown(1, 120, commands.BucketType.user)
+@commands.cooldown(1, 600, commands.BucketType.user)
 async def mail(ctx, alici, *, içerik):
     port = 465 
     smtp_server = "smtp.gmail.com"
@@ -2510,5 +2510,55 @@ async def hi(message):
             await message.channel.send("<:goz:859374017264746516>")            
 
 # Otamatik Cevaplar Sonu     
+
+@bot.command()
+@commands.cooldown(1, 15, commands.BucketType.user)
+async def sigarahesapla(ctx):
+    pakette_kac_sigara = "20"
+
+    def is_correct(m):
+        return m.author == ctx.author and m.content.isdigit()
+
+    await ctx.send('**<a:sigara:854437179501182996> Kaç yıldır sigara kullanıyorsunuz?**')  
+        
+    try:
+        kac_yil = await bot.wait_for('message', check=is_correct, timeout=10.0)
+    except asyncio.TimeoutError:
+        return await ctx.send(f'**<:normalcarpi:852958720328466474> Verilerini belirtmen çok uzun sürdü. Daha sonra tekrar dene.**')
+
+    await ctx.send('**<a:sigara:854437179501182996> Bir günde kaç tane sigara içiyorsunuz?** ')  
+
+    try:
+        gunde_kac_tane = await bot.wait_for('message', check=is_correct, timeout=10.0)
+    except asyncio.TimeoutError:
+        return await ctx.send(f'**<:normalcarpi:852958720328466474> Verilerini belirtmen çok uzun sürdü. Daha sonra tekrar dene.**') 
+        
+    await ctx.send('**<a:sigara:854437179501182996> Bir paket sigaraya kaç lira harcıyorsunuz? (Eğer bu değer yıldan yıla farklılık göseriyorsa lütfen ortalama bir değer giriniz)** ')  
+
+    try:
+        paket_fiyati = await bot.wait_for('message', check=is_correct, timeout=10.0)
+    except asyncio.TimeoutError:
+        return await ctx.send(f'**<:normalcarpi:852958720328466474> Verilerini belirtmen çok uzun sürdü. Daha sonra tekrar dene.**')   
+
+    embed = discord.Embed(title="<a:sigara:854437179501182996> Sigara Zarar Hesaplama Komutu", description=f"Girdiğiniz verilere göre, sigaradan ne kadar zarar ettiğiniz size detaylı bir rapor halinde sunulur.", color=0x00ff33)
+    embed.add_field(name=f"<a:sigara:854437179501182996> Bir Haftada İçtiğiniz Toplam Sigara Sayısı", value=f"{int(gunde_kac_tane.content) * 7} Tane", inline=True)
+    embed.add_field(name=f"<a:sigara:854437179501182996> Bir Haftada Sigaraya Harcadığınız Toplam Para", value=f"{int(gunde_kac_tane.content) / int(pakette_kac_sigara.content) * int(paket_fiyati.content) * 7}", inline=True)    
+    
+    embed.add_field(name=f"<a:sigara:854437179501182996> Bir Ayda İçtiğiniz Toplam Sigara Sayısı", value=f"{int(gunde_kac_tane.content) * 30} Tane", inline=True)
+    embed.add_field(name=f"<a:sigara:854437179501182996> Bir Ayda Sigaraya Harcadığınız Toplam Para", value=f"{int(gunde_kac_tane.content) / int(pakette_kac_sigara.content) * int(paket_fiyati.content) * 30} TL", inline=True)   
+
+    embed.add_field(name=f"<a:sigara:854437179501182996> Bir Yılda İçtiğiniz Toplam Sigara Sayısı", value=f"{int(gunde_kac_tane.content) * 365} Tane", inline=True)
+    embed.add_field(name=f"<a:sigara:854437179501182996> Bir Yılda Sigaraya Harcadığınız Toplam Para", value=f"{int(gunde_kac_tane.content) / int(pakette_kac_sigara.content) * int(paket_fiyati.content) * 365} TL", inline=True)   
+
+    embed.add_field(name=f"<a:sigara:854437179501182996> Girdiğiniz Verilere Göre {kac_yil} Yılda İçtiğiniz Toplam Sigara Sayısı", value=f"{int(gunde_kac_tane.content) * int(kac_yil.content) * 365} Tane", inline=True)
+    embed.add_field(name=f"<a:sigara:854437179501182996> Girdiğiniz Verilere Göre {kac_yil} Yılda Sigaraya Harcadığınız Toplam Para", value=f"{int(gunde_kac_tane.content) / int(pakette_kac_sigara.content) * int(paket_fiyati.content) * int(kac_yil.content) * 365} TL", inline=True)       
+
+    await ctx.send(embed=embed)
+
+@sigarahesapla.error
+async def sigarahesapla_error(ctx, error): 
+    if isinstance(error, commands.CommandOnCooldown):
+        await ctx.send(f'<:saat:867471142800457748> **Bu komutu kullanabilmek için {round(error.retry_after, 2)} saniye daha beklmelisin.**')
+
 
 bot.run(TOKEN)
