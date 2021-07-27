fa0e580ef3b25f9bd500c6818f2ad6b1796d6805
Weyaxi <81961593+Weyaxi@users.noreply.github.com>
2021-07-27 12:52:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1646,7 +1646,7 @@ async def işeyarar(ctx):
     
     embed = discord.Embed(
         title="▬▬▬▬▬▬▬[ 🔐 İşe Yarar Komutlar 🔐  ]▬▬▬▬▬▬",
-        description="> <:yesilok:843149816880037899> **w!discordnedir:** Discord hakkında bilgiler size sunulur. \n > _ _ \n > <:yesilok:843149816880037899> **w!telegramnedir:** Telegram hakkında bazı bilgileri size sunulur. \n  > _ _ \n > <:yesilok:843149816880037899> **w!instagramnedir:** İnstagram hakkında bazı bilgileri size sunulur. \n  > _ _ \n > <:yesilok:843149816880037899> **w!facebooknedir:** Facebook hakkında bazı bilgileri size sunulur. \n  > _ _ \n > <:yesilok:843149816880037899> **w!twitternedir:** Twitter hakkında bazı bilgileri size sunulur. \n  > _ _ \n > <:yesilok:843149816880037899> **w!whatsappnedir:** Whatsapp hakkında bazı bilgileri size sunulur. \n  > _ _ \n > <:yesilok:843149816880037899> **w!youtubenedir:** Youtube hakkında bazı bilgileri size sunulur. \n  > _ _ \n > <:yesilok:843149816880037899> **w!twitchnedir:** Twitch hakkında bazı bilgileri size sunulur. \n  > _ _ \n > <:yesilok:843149816880037899> **w!rozetler:** Bütün Discord rozetleri, size renkli bir şekilde sunulur. \n  > _ _ \n > <:yesilok:843149816880037899> **w!nitro:** Discord Nitro seçenekleri size sunulur. \n  > _ _ \n > <:yesilok:843149816880037899> **w!botudavetet:** Komut sonrasında belirttiğiniz botun davet linklerini size sunar. \n  > _ _ \n > <:yesilok:843149816880037899> **w!linkkısalt:** Bot, komut sonrasında belirttiğiniz linki kolay bir şekilde kısaltır. \n  > _ _ \n > <:yesilok:843149816880037899> **w!hackaraçları:** Bot, bazı yaygın hack araçlarını size sunar. (Sorumluluk kabul etmiyorum) \n  > _ _ \n > <:yesilok:843149816880037899> **w!önemligünler:** Belli başlı önemli günler size sunulur. \n > _ _ \n > <:yesilok:843149816880037899> **w!covid:** Bütün Dünya'da şuana kadarki güncel corona virüs verileri size gösterilir. ",
+        description="> <:yesilok:843149816880037899> **w!discordnedir:** Discord hakkında bilgiler size sunulur. \n > _ _ \n > <:yesilok:843149816880037899> **w!telegramnedir:** Telegram hakkında bazı bilgileri size sunulur. \n  > _ _ \n > <:yesilok:843149816880037899> **w!instagramnedir:** İnstagram hakkında bazı bilgileri size sunulur. \n  > _ _ \n > <:yesilok:843149816880037899> **w!facebooknedir:** Facebook hakkında bazı bilgileri size sunulur. \n  > _ _ \n > <:yesilok:843149816880037899> **w!twitternedir:** Twitter hakkında bazı bilgileri size sunulur. \n  > _ _ \n > <:yesilok:843149816880037899> **w!whatsappnedir:** Whatsapp hakkında bazı bilgileri size sunulur. \n  > _ _ \n > <:yesilok:843149816880037899> **w!youtubenedir:** Youtube hakkında bazı bilgileri size sunulur. \n  > _ _ \n > <:yesilok:843149816880037899> **w!twitchnedir:** Twitch hakkında bazı bilgileri size sunulur. \n  > _ _ \n > <:yesilok:843149816880037899> **w!rozetler:** Bütün Discord rozetleri, size renkli bir şekilde sunulur. \n  > _ _ \n > <:yesilok:843149816880037899> **w!nitro:** Discord Nitro seçenekleri size sunulur. \n  > _ _ \n > <:yesilok:843149816880037899> **w!botudavetet:** Komut sonrasında belirttiğiniz botun davet linklerini size sunar. \n  > _ _ \n > <:yesilok:843149816880037899> **w!linkkısalt:** Bot, komut sonrasında belirttiğiniz linki kolay bir şekilde kısaltır. \n  > _ _ \n > <:yesilok:843149816880037899> **w!hackaraçları:** Bot, bazı yaygın hack araçlarını size sunar. (Sorumluluk kabul etmiyorum) \n  > _ _ \n > <:yesilok:843149816880037899> **w!önemligünler:** Belli başlı önemli günler size sunulur. \n > _ _ \n > <:yesilok:843149816880037899> **w!covid:** Bütün Dünya'da şuana kadarki güncel corona virüs verileri size gösterilir. n > _ _ \n > <:yesilok:843149816880037899> **w!sigarahesapla:** Girdiğiniz verilere göre, sigaradan ne kadar zarar ettiğiniz size detaylı bir rapor halinde sunulur.",
         color=discord.Color.blue()
     )
     embed.set_author(name=ctx.author.display_name, url="", icon_url=ctx.author.avatar_url)
@@ -1654,6 +1654,55 @@ async def işeyarar(ctx):
 
 
     await ctx.send(embed=embed)      
+
+@bot.command()
+@commands.cooldown(1, 15, commands.BucketType.user)
+async def sigarahesapla(ctx):
+    pakette_kac_sigara = "20"
+
+    def is_correct(m):
+        return m.author == ctx.author and m.content.isdigit()
+
+    await ctx.send('**<a:sigara:854437179501182996> Kaç yıldır sigara kullanıyorsunuz?**')  
+        
+    try:
+        kac_yil = await bot.wait_for('message', check=is_correct, timeout=10.0)
+    except asyncio.TimeoutError:
+        return await ctx.send(f'**<:normalcarpi:852958720328466474> Verilerini belirtmen çok uzun sürdü. Daha sonra tekrar dene.**')
+
+    await ctx.send('**<a:sigara:854437179501182996> Bir günde kaç tane sigara içiyorsunuz?** ')  
+
+    try:
+        gunde_kac_tane = await bot.wait_for('message', check=is_correct, timeout=10.0)
+    except asyncio.TimeoutError:
+        return await ctx.send(f'**<:normalcarpi:852958720328466474> Verilerini belirtmen çok uzun sürdü. Daha sonra tekrar dene.**') 
+        
+    await ctx.send('**<a:sigara:854437179501182996> Bir paket sigaraya kaç lira harcıyorsunuz? (Eğer bu değer yıldan yıla farklılık göseriyorsa lütfen ortalama bir değer giriniz)** ')  
+
+    try:
+        paket_fiyati = await bot.wait_for('message', check=is_correct, timeout=10.0)
+    except asyncio.TimeoutError:
+        return await ctx.send(f'**<:normalcarpi:852958720328466474> Verilerini belirtmen çok uzun sürdü. Daha sonra tekrar dene.**')   
+
+    embed = discord.Embed(title="<a:sigara:854437179501182996> Sigara Zarar Hesaplama Komutu", description=f"Girdiğiniz verilere göre, sigaradan ne kadar zarar ettiğiniz size detaylı bir rapor halinde sunulur.", color=62150)
+    embed.add_field(name=f"<a:sigara:854437179501182996> Bir Haftada İçtiğiniz Toplam Sigara Sayısı", value=f"{int(gunde_kac_tane.content) * 7} Tane", inline=True)
+    embed.add_field(name=f"<a:sigara:854437179501182996> Bir Haftada Sigaraya Harcadığınız Toplam Para", value=f"{int(gunde_kac_tane.content) / 20 * int(paket_fiyati.content) * 7}", inline=True)    
+    
+    embed.add_field(name=f"<a:sigara:854437179501182996> Bir Ayda İçtiğiniz Toplam Sigara Sayısı", value=f"{int(gunde_kac_tane.content) * 30} Tane", inline=True)
+    embed.add_field(name=f"<a:sigara:854437179501182996> Bir Ayda Sigaraya Harcadığınız Toplam Para", value=f"{int(gunde_kac_tane.content) / 20 * int(paket_fiyati.content) * 30} TL", inline=True)   
+
+    embed.add_field(name=f"<a:sigara:854437179501182996> Bir Yılda İçtiğiniz Toplam Sigara Sayısı", value=f"{int(gunde_kac_tane.content) * 365} Tane", inline=True)
+    embed.add_field(name=f"<a:sigara:854437179501182996> Bir Yılda Sigaraya Harcadığınız Toplam Para", value=f"{int(gunde_kac_tane.content) / 20 * int(paket_fiyati.content) * 365} TL", inline=True)   
+
+    embed.add_field(name=f"<a:sigara:854437179501182996> {kac_yil.content} Yılda İçtiğiniz Toplam Sigara Sayısı", value=f"{int(gunde_kac_tane.content) * int(kac_yil.content) * 365} Tane", inline=False)
+    embed.add_field(name=f"<a:sigara:854437179501182996> {kac_yil.content} Yılda Sigaraya Harcadığınız Toplam Para", value=f"{int(gunde_kac_tane.content) / 20 * int(paket_fiyati.content) * int(kac_yil.content) * 365} TL", inline=True)       
+
+    await ctx.send(embed=embed)
+
+@sigarahesapla.error
+async def sigarahesapla_error(ctx, error): 
+    if isinstance(error, commands.CommandOnCooldown):
+        await ctx.send(f'<:saat:867471142800457748> **Bu komutu kullanabilmek için {round(error.retry_after, 2)} saniye daha beklmelisin.**')
 
 @bot.command()
 async def embeds(ctx):
@@ -2510,54 +2559,5 @@ async def hi(message):
             await message.channel.send("<:goz:859374017264746516>")            
 
 # Otamatik Cevaplar Sonu     
-
-@bot.command()
-@commands.cooldown(1, 15, commands.BucketType.user)
-async def sigarahesapla(ctx):
-    pakette_kac_sigara = "20"
-
-    def is_correct(m):
-        return m.author == ctx.author and m.content.isdigit()
-
-    await ctx.send('**<a:sigara:854437179501182996> Kaç yıldır sigara kullanıyorsunuz?**')  
-        
-    try:
-        kac_yil = await bot.wait_for('message', check=is_correct, timeout=10.0)
-    except asyncio.TimeoutError:
-        return await ctx.send(f'**<:normalcarpi:852958720328466474> Verilerini belirtmen çok uzun sürdü. Daha sonra tekrar dene.**')
-
-    await ctx.send('**<a:sigara:854437179501182996> Bir günde kaç tane sigara içiyorsunuz?** ')  
-
-    try:
-        gunde_kac_tane = await bot.wait_for('message', check=is_correct, timeout=10.0)
-    except asyncio.TimeoutError:
-        return await ctx.send(f'**<:normalcarpi:852958720328466474> Verilerini belirtmen çok uzun sürdü. Daha sonra tekrar dene.**') 
-        
-    await ctx.send('**<a:sigara:854437179501182996> Bir paket sigaraya kaç lira harcıyorsunuz? (Eğer bu değer yıldan yıla farklılık göseriyorsa lütfen ortalama bir değer giriniz)** ')  
-
-    try:
-        paket_fiyati = await bot.wait_for('message', check=is_correct, timeout=10.0)
-    except asyncio.TimeoutError:
-        return await ctx.send(f'**<:normalcarpi:852958720328466474> Verilerini belirtmen çok uzun sürdü. Daha sonra tekrar dene.**')   
-
-    embed = discord.Embed(title="<a:sigara:854437179501182996> Sigara Zarar Hesaplama Komutu", description=f"Girdiğiniz verilere göre, sigaradan ne kadar zarar ettiğiniz size detaylı bir rapor halinde sunulur.", color=62150)
-    embed.add_field(name=f"<a:sigara:854437179501182996> Bir Haftada İçtiğiniz Toplam Sigara Sayısı", value=f"{int(gunde_kac_tane.content) * 7} Tane", inline=True)
-    embed.add_field(name=f"<a:sigara:854437179501182996> Bir Haftada Sigaraya Harcadığınız Toplam Para", value=f"{int(gunde_kac_tane.content) / 20 * int(paket_fiyati.content) * 7}", inline=True)    
-    
-    embed.add_field(name=f"<a:sigara:854437179501182996> Bir Ayda İçtiğiniz Toplam Sigara Sayısı", value=f"{int(gunde_kac_tane.content) * 30} Tane", inline=True)
-    embed.add_field(name=f"<a:sigara:854437179501182996> Bir Ayda Sigaraya Harcadığınız Toplam Para", value=f"{int(gunde_kac_tane.content) / 20 * int(paket_fiyati.content) * 30} TL", inline=True)   
-
-    embed.add_field(name=f"<a:sigara:854437179501182996> Bir Yılda İçtiğiniz Toplam Sigara Sayısı", value=f"{int(gunde_kac_tane.content) * 365} Tane", inline=True)
-    embed.add_field(name=f"<a:sigara:854437179501182996> Bir Yılda Sigaraya Harcadığınız Toplam Para", value=f"{int(gunde_kac_tane.content) / 20 * int(paket_fiyati.content) * 365} TL", inline=True)   
-
-    embed.add_field(name=f"<a:sigara:854437179501182996> {kac_yil.content} Yılda İçtiğiniz Toplam Sigara Sayısı", value=f"{int(gunde_kac_tane.content) * int(kac_yil.content) * 365} Tane", inline=False)
-    embed.add_field(name=f"<a:sigara:854437179501182996> {kac_yil.content} Yılda Sigaraya Harcadığınız Toplam Para", value=f"{int(gunde_kac_tane.content) / 20 * int(paket_fiyati.content) * int(kac_yil.content) * 365} TL", inline=True)       
-
-    await ctx.send(embed=embed)
-
-@sigarahesapla.error
-async def sigarahesapla_error(ctx, error): 
-    if isinstance(error, commands.CommandOnCooldown):
-        await ctx.send(f'<:saat:867471142800457748> **Bu komutu kullanabilmek için {round(error.retry_after, 2)} saniye daha beklmelisin.**')
 
 bot.run(TOKEN)
