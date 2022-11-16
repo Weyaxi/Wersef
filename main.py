d155e04490b8cb6500f2743d4466c8928e1428c5
Weyaxi <81961593+Weyaxi@users.noreply.github.com>
2022-11-16 18:10:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2655,5 +2655,48 @@ async def wikipedia_ara(ctx):
 
 # birden fazla anlam olabilecek şeye bak sonra. mesela merhaba sözcüğü 5 tane makale falan var. Anladın? #
 # error handling bir de #
+url = "https://discord.com/api/v10/applications/819743355663548447/commands"
+
+# This is an example CHAT_INPUT or Slash Command, with a type of 1
+json = {
+    "name": "blep",
+    "type": 1,
+    "description": "Send a random adorable animal photo",
+    "options": [
+        {
+            "name": "animal",
+            "description": "The type of animal",
+            "type": 3,
+            "required": True,
+            "choices": [
+                {
+                    "name": "Dog",
+                    "value": "animal_dog"
+                },
+                {
+                    "name": "Cat",
+                    "value": "animal_cat"
+                },
+                {
+                    "name": "Penguin",
+                    "value": "animal_penguin"
+                }
+            ]
+        },
+        {
+            "name": "only_smol",
+            "description": "Whether to show only baby animals",
+            "type": 5,
+            "required": False
+        }
+    ]
+}
+
+# For authorization, you can use either your bot token
+headers = {
+    "Authorization": "Bot ODE5NzQzMzU1NjYzNTQ4NDQ3.YErDfg.NQJNCdgMV3JEVUcsmYXBeDg7q3A"
+}
+
+r = requests.post(url, headers=headers, json=json)
 
 bot.run(TOKEN)
